df690de456dcf7904e8b73bf28f0bfc449e09c07
221V <221v92@gmail.com>
2015-10-22 01:29:57 +0300

    hello_name

---
+++ hello2.erl
@@ -0,0 +1,9 @@
+-module(hello2).
+-compile(export_all).
+
+greet(male, Name) ->
+	io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+	io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+	io:format("Hello, ~s!", [Name]).
