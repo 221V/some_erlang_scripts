fd194b60905392e7291715b231627d392b318691
221V <221v92@gmail.com>
2015-10-22 01:56:20 +0300

    case end

---
+++ case_end.erl
@@ -0,0 +1,14 @@
+-module(case_end).
+-compile(export_all).
+
+asst_temp(Temp) ->
+	case Temp of
+		{X, celsius} when 20 =< X, X =< 45 ->
+			'favorable';
+		{X, kelvin} when 293 =< X, X =< 318 ->
+			'scientifically favorable';
+		{X, fahrenheit} when 68 =< X, X =< 113 ->
+			'favorable in the US';
+		_ ->
+			'not the best tempperature'
+	end.
